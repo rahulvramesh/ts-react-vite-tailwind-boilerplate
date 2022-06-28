c7f48d00eea3dbb955affcc87c96767a81cb4864
Rahul VR <rahulvramesh@hotmail.com>
2022-06-28 11:10:49 +0530

    sentry

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Key, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import * as Sentry from '@sentry/react';
+
 import { staticRoutes, protectedRoutes } from './routes';
 import RequireAuth from './routes/route';
 
@@ -9,43 +11,49 @@ import AuthLayout from './layouts/AuthLayout';
 import MainLayout from './layouts/MainLayout';
 
 import PageNotFound from './pages/PageNotFound';
+import FallbackComponent from './components/FallbackComponent';
 
 import './App.css';
+
+/* Feedback Component */
+const myFallback = <FallbackComponent message={'Something Happened!'} />;
 
 function App() {
   return (
     <>
-      <Helmet>
-        <title>Login - Highway.so</title>
-        <meta name="description" content="Free Web tutorials" />
-        <meta name="keywords" content="HTML, CSS, JavaScript" />
-      </Helmet>
+      <Sentry.ErrorBoundary fallback={myFallback} showDialog>
+        <Helmet>
+          <title>Login - Highway.so</title>
+          <meta name="description" content="Free Web tutorials" />
+          <meta name="keywords" content="HTML, CSS, JavaScript" />
+        </Helmet>
 
-      <Routes>
-        {staticRoutes.map(
-          (route: { path: any; component: any }, idx: Key | null | undefined) => (
-            <Route
-              key={idx}
-              path={route.path}
-              element={<AuthLayout>{route.component()}</AuthLayout>}
-            />
-          )
-        )}
-        {protectedRoutes.map(
-          (route: { path: any; component: any }, idx: Key | null | undefined) => (
-            <Route
-              key={idx}
-              path={route.path}
-              element={
-                <RequireAuth>
-                  <MainLayout>{route.component()}</MainLayout>
-                </RequireAuth>
-              }
-            />
-          )
-        )}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+        <Routes>
+          {staticRoutes.map(
+            (route: { path: any; component: any }, idx: Key | null | undefined) => (
+              <Route
+                key={idx}
+                path={route.path}
+                element={<AuthLayout>{route.component()}</AuthLayout>}
+              />
+            )
+          )}
+          {protectedRoutes.map(
+            (route: { path: any; component: any }, idx: Key | null | undefined) => (
+              <Route
+                key={idx}
+                path={route.path}
+                element={
+                  <RequireAuth>
+                    <MainLayout>{route.component()}</MainLayout>
+                  </RequireAuth>
+                }
+              />
+            )
+          )}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Sentry.ErrorBoundary>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 
 import App from './App';
 // @ts-ignore
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
+
+if (import.meta.env.VITE_ENABLE_SENTRY) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 0.2,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthProvider>
